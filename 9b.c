34e82ae0dd1ea1a0564401db7481f480e5d32ed2
madhan076 <24B11DS002@adityauniversity.in>
2025-10-16 14:39:53 +0530

    Create 9b.c

---
+++ 9b.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int findmin(int[],int);
+int findmax(int[],int);
+int findmin(int arr[],int size);
+{
+	int min=0,i;
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]<min)
+		min=arr[i];
+		return min;
+	}
+	int findmax(int arr[],size);
+	{
+		int max=0;i;
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i]>max)
+			max=arr[i];
+			return max;
+		}
+	}
+	int main()
+	min=findmin(int arr[],int);
+	max=findmax(int arr[],int);
+	printf("The minimum element is:%d",min);
+	printf("The maximum element is:%d",max);
+	int i,arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",arr[i]);
+	}
+}
