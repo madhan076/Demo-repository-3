a1b2f4bd6af7ac4970f44a52fd322bacf6fc33a5
madhan076 <24B11DS002@adityauniversity.in>
2025-10-16 14:40:29 +0530

    Create Arrays.3c.cpp

---
+++ Arrays.3c.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int arr[4]={1,2,3,4};
+	printf("before changing = arr[1]",arr[1]);
+    int	beforechanging= 2;
+	printf("after changing = arr[1]",arr[1]);
+	int afterchanging = 20;
+}
